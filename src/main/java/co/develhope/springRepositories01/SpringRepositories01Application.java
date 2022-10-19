961089201ceed118e86afa94678fa77d5ab5ebb9
Giuseppe Corrao <josephcorr99@gmail.com>
2022-10-19 18:41:31 +0200

    initial commit

---
+++ src/main/java/co/develhope/springRepositories01/SpringRepositories01Application.java
@@ -0,0 +1,13 @@
+package co.develhope.springRepositories01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRepositories01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRepositories01Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/develhope/springRepositories01/SpringRepositories01ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.springRepositories01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRepositories01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
